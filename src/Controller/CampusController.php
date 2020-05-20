f5feaba6ba3bddcecccfd5942d6b07f25e059b6b
tlize <tlize@msn.com>
2020-05-20 13:46:00 +0200

    generation crud campus

---
+++ src/Controller/CampusController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Campus;
+use App\Form\CampusType;
+use App\Repository\CampusRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/campus")
+ */
+class CampusController extends AbstractController
+{
+    /**
+     * @Route("/", name="campus_index", methods={"GET"})
+     */
+    public function index(CampusRepository $campusRepository): Response
+    {
+        return $this->render('campus/index.html.twig', [
+            'campuses' => $campusRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="campus_new", methods={"GET","POST"})
+     */
+    public function new(Request $request): Response
+    {
+        $campu = new Campus();
+        $form = $this->createForm(CampusType::class, $campu);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($campu);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('campus_index');
+        }
+
+        return $this->render('campus/new.html.twig', [
+            'campu' => $campu,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="campus_show", methods={"GET"})
+     */
+    public function show(Campus $campu): Response
+    {
+        return $this->render('campus/show.html.twig', [
+            'campu' => $campu,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="campus_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, Campus $campu): Response
+    {
+        $form = $this->createForm(CampusType::class, $campu);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('campus_index');
+        }
+
+        return $this->render('campus/edit.html.twig', [
+            'campu' => $campu,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="campus_delete", methods={"DELETE"})
+     */
+    public function delete(Request $request, Campus $campu): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$campu->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($campu);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('campus_index');
+    }
+}
---
+++ src/Form/ParticipantType.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Campus;
+use App\Entity\Participant;
+use App\Entity\Sortie;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ParticipantType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nom')
+            ->add('prenom')
+            ->add('telephone')
+            ->add('mail')
+            ->add('mot_de_passe', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'Les deux champs doivent correspondre !',
+                'required' => true,
+                'first_options'  => ['label' => 'Mot de passe'],
+                'second_options' => ['label' => 'Confirmer le mot de passe']
+            ])
+            ->add('campus', EntityType::class,[
+                'class'=>Campus::class,
+                'choice_label' => 'nom',
+                'label' => 'Campus :'])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Participant::class,
+        ]);
+    }
+}
